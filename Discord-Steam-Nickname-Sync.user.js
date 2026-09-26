de4bd73485d84da5710fcd3d84e5ea1d0cdbcd15
Gump <dennis.f.glenn@gmail.com>
2026-09-26 18:44:11 -0400

    Bump userscript version for Steam Friends URL fix

--- Discord-Steam-Nickname-Sync.user.js
+++ Discord-Steam-Nickname-Sync.user.js
@@ -2,7 +2,7 @@
 
 // @name         Discord Steam Nickname Sync
 // @namespace    discord-steam-sync
-// @version      10.2.1
+// @version      10.2.2
 // @description  Sync Steam friend local nicknames from Discord roles.
 // @homepageURL  https://github.com/RustyGumpers/steam-link
 // @supportURL   https://github.com/RustyGumpers/steam-link/issues
